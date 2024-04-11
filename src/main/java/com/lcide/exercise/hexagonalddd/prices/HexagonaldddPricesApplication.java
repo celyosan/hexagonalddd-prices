010fcab33731e39f79cb332bb3549ebd6c41f2ef
Luis Cid Encinar <lcidenc@corp.vodafone.es>
2024-04-11 18:37:36 +0200

    First commit

---
+++ src/main/java/com/lcide/exercise/hexagonalddd/prices/HexagonaldddPricesApplication.java
@@ -0,0 +1,13 @@
+package com.lcide.exercise.hexagonalddd.prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonaldddPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonaldddPricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcide/exercise/hexagonalddd/prices/HexagonaldddPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcide.exercise.hexagonalddd.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonaldddPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
